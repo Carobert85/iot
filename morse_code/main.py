2975d473280c4b1b9ba7c20198179e95324d97a8
Carobert85 <43256758+Carobert85@users.noreply.github.com>
2022-07-12 07:13:28 -0700

    feat: morse code flasher

---
+++ morse_code/main.py
@@ -0,0 +1,31 @@
+import machine
+from time import sleep
+
+# Dictionary representing the morse code chart
+MORSE_CODE_DICT = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
+                   'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
+                   'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+                   'Y': '-.--', 'Z': '--..', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....',
+                   '6': '-....', '7': '--...', '8': '---..', '9': '----.', '0': '-----', ', ': '--..--', '.': '.-.-.-',
+                   '?': '..--..', '/': '-..-.', '-': '-....-', '(': '-.--.', ')': '-.--.-'}
+MORSE_CODE_TIMES = {'.': 1, '-': 2}
+
+morse_pin = machine.Pin(16, machine.Pin.OUT)
+
+
+def morse_code(phrase, pin):
+    for word in phrase.split():  # splits our phrase into words
+        for letter in word:
+            code = MORSE_CODE_DICT.get(letter.upper())
+            for symbol in code:
+                pin.value(0)
+                sleep(MORSE_CODE_TIMES.get(symbol))
+                pin.value(1)
+                sleep(1) # Sleep for 1 second between dots and dashes
+            sleep(2)  # Sleep for 3 seconds between letters (includes 1 second from end of dot and dash)
+        sleep(4)  # Sleep for 7 seconds between words (includes 3 seconds from end of letter)
+    return True
+
+sleep(1)
+morse_code('I love you, Meg!', morse_pin)
+
